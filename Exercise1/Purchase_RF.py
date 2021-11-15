7f964326d885f04d06099dfc16195fb3ea003d88
Robin Wroblowski <robin.wroblowski@telenet.be>
2021-11-15 12:47:53 +0100

    RF?

--- Exercise1/Purchase_RF.py
+++ Exercise1/Purchase_RF.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Purchase_SVM.ipynb
+"""Purchase_RF.ipynb
 """
 
 import pandas as pd
@@ -7,6 +7,8 @@ import numpy as np
 import pathlib
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+from tqdm import tqdm
 from sklearn import svm
 import timeit
 from sklearn.model_selection import cross_val_score
@@ -15,14 +17,14 @@ import matplotlib.pyplot as plt
 
 # ---------------- DEFINE FUNCTIONS ----------------
 
-# Train the SVM on the training data and predict using the test data
-def predict_svm(x_te, x_tr, y_tr):
-    every_predictions = []
-    svc = svm.SVC()
-    svc.fit(x_tr, y_tr)
-    prediction = svc.predict(x_te)
-    every_predictions.append(prediction)
-    return every_predictions
+def predict_random_forest(x_te, x_tr, y_tr, k_min=70, k_max=100):
+    every_prediction = []
+    for k in tqdm(range(k_min, k_max)):
+        rndf = RandomForestClassifier(n_estimators=k, criterion="entropy")
+        rndf.fit(x_tr, y_tr)
+        prediction = rndf.predict(x_te)
+        every_prediction.append(prediction)
+    return every_prediction
 
 
 # Check the accuracy of given predictions on the test set y_test
@@ -74,89 +76,53 @@ df_test_normalized = pd.DataFrame(x_test_scaled)
 r = []
 t = []
 runtime = []
+testSizeRange = list(range(7, 13, 2))
 
-print("\nTRAINING USING SVM")
-for testSize in range(5, 17, 2):
-    X_train, X_test, Y_train, Y_test = train_test_split(df_x_scaled, Y, test_size=testSize/100, random_state=35)
+print("\nTRAINING USING RANDOM FOREST")
+for testSize in testSizeRange:
+    X_train, X_test, Y_train, Y_test = train_test_split(df_x_scaled, Y, test_size=testSize / 100, random_state=35)
 
-    # SUPPORT VECTOR MACHINES
+    # RANDOM FORESTS
     start = timeit.default_timer()
-    all_predictions = predict_svm(X_test, X_train, Y_train)
+    all_predictions = predict_random_forest(X_test, X_train, Y_train)
     stop = timeit.default_timer()
-    time = stop-start
+    time = stop - start
     print('Time: ', time)
     results = check_accuracy(Y_test, all_predictions)
-    print("\nTest size = ", testSize/100)
+    print("\nTest size = ", testSize / 100)
     print(results)
-    testSize = testSize/100
+    testSize = testSize / 100
     r.append(results)
     t.append(testSize)
     runtime.append(time)
 
-best_testSize = t[r.index(np.max(r))]
+bestOverallResult = np.max(r)
+df = pd.DataFrame(data=r)
+idxs = df.stack().index[np.argmax(df.values)]
+print("The best indexes (testsize, k): ", idxs)
+
+fig = df.plot(title='Training with different k and test sizes', ylabel='Accuracy', xlabel='K')
+fig.legend(testSizeRange)
+# fig.subtitle('SVM with different test sizes', fontsize=14)
+# df.xlabel('Test sie', fontsize=14)
+# df.ylabel('Accuracy', fontsize=14)
+# print("Max accuracy = ", np.max(r), "with testsize = ", best_testSize)
 
 fig = plt.figure()
-plt.scatter(t, r)
-fig.suptitle('SVM with different test sizes', fontsize=14)
-plt.xlabel('Test sie', fontsize=14)
-plt.ylabel('Accuracy', fontsize=14)
-print("Max accuracy = ", np.max(r), "with testsize = ", best_testSize)
+plt.scatter(testSizeRange, runtime)
+fig.suptitle('Runtime', fontsize=14)
+plt.xlabel('Test size', fontsize=14)
+plt.ylabel('Run time', fontsize=14)
 
-
-fig, ax1 = plt.subplots()
-
-color = 'tab:red'
-ax1.set_xlabel('Test size')
-ax1.set_ylabel('Accuracy', color=color)
-ax1.plot(t, r, color=color)
-ax1.tick_params(axis='y', labelcolor=color)
-
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-
-color = 'tab:blue'
-ax2.set_ylabel('runtime', color=color)  # we already handled the x-label with ax1
-ax2.plot(t, runtime, color=color)
-ax2.tick_params(axis='y', labelcolor=color)
-
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
-plt.show()
-
-cv_ = int((100/(best_testSize*100)))
-testSize = 100/cv_
-
-clf = svm.SVC(kernel='linear', C=1, random_state=42)
+cv_ = int((idxs[0] + 1))
+testSize = 100 / cv_
+clf = RandomForestClassifier(n_estimators=idxs[1] + 1)
 scores = cross_val_score(clf, X, Y, cv=cv_)
-x = range(1,cv_+1)
+x = range(1, cv_ + 1)
 
 fig = plt.figure()
-plt.scatter(x,scores)
-fig.suptitle('SVM cross validation with test size ' + str(testSize) + '% with linear kernel', fontsize=14)
-plt.xlabel('Iteration', fontsize=14)
-plt.ylabel('Accuracy', fontsize=14)
-print("Max accuracy = ", np.max(scores))
-
-cv_ = int((100/(best_testSize*100)))
-
-clf = svm.SVC(kernel='rbf', C=1, random_state=42)
-scores = cross_val_score(clf, X, Y, cv=cv_)
-x = range(1,cv_+1)
-
-fig = plt.figure()
-plt.scatter(x,scores)
-fig.suptitle('SVM cross validation with test size ' + str(testSize) + '% with rbf kernel', fontsize=14)
-plt.xlabel('Iteration', fontsize=14)
-plt.ylabel('Accuracy', fontsize=14)
-print("Max accuracy = ", np.max(scores))
-
-cv_ = int((100/(best_testSize*100)))
-
-clf = svm.SVC(kernel='poly', C=1, random_state=42)
-scores = cross_val_score(clf, X, Y, cv=cv_)
-x = range(1,cv_+1)
-
-fig = plt.figure()
-plt.scatter(x,scores)
-fig.suptitle('SVM cross validation with test size ' + str(testSize) + '% with polynomial kernel', fontsize=14)
+plt.scatter(x, scores)
+fig.suptitle('RF cross validation with test size ' + str(testSize), fontsize=14)
 plt.xlabel('Iteration', fontsize=14)
 plt.ylabel('Accuracy', fontsize=14)
 print("Max accuracy = ", np.max(scores))
