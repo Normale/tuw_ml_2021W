099517747d55f67a8970efeb1132bf483163aec1
Normal <bartek.dybcio1@gmail.com>
2021-12-13 20:43:48 +0100

    Add all tpot files

--- Exercise2/Models/tpot_Wine_best_model2.py
+++ Exercise2/Models/tpot_Wine_best_model2.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator
+from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
+from sklearn.svm import LinearSVR
+from tpot.builtins import OneHotEncoder, StackingEstimator
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -13,10 +15,16 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=1)
 
-# Average CV score on the training set was: -1.1086236171111754
+# Average CV score on the training set was: -0.5422980179962902
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.9, learning_rate=0.01, loss="huber", max_depth=2, max_features=0.35000000000000003, min_samples_leaf=19, min_samples_split=15, n_estimators=100, subsample=0.8)),
-    LassoLarsCV(normalize=True)
+    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
+    StackingEstimator(estimator=LinearSVR(C=0.01, dual=True, epsilon=0.001, loss="squared_epsilon_insensitive", tol=0.1)),
+    MinMaxScaler(),
+    MinMaxScaler(),
+    OneHotEncoder(minimum_fraction=0.05, sparse=False, threshold=10),
+    StackingEstimator(estimator=LassoLarsCV(normalize=True)),
+    StackingEstimator(estimator=LassoLarsCV(normalize=False)),
+    ExtraTreesRegressor(bootstrap=False, max_features=0.2, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 1)
