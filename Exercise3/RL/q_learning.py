23370eaf51171d02574d4b0192a69d0f00483c29
Normal <bartek.dybcio1@gmail.com>
2022-01-05 15:00:41 +0100

    Add visualisation

--- Exercise3/RL/q_learning.py
+++ Exercise3/RL/q_learning.py
@@ -1,13 +1,21 @@
 import numpy as np
 from .environment import Environment
 from time import time, sleep
+from pathlib import Path
 
-EPISODES = 40000
+EPISODES = 50000
 DISCOUNT = 0.9
 LEARNING_RATE = 0.4
 EPSILON = 0.5
 
 q_table = np.zeros(shape = (3,3,3,3,3,3,3,3,3,9))
+
+
+
+draw_win_lose = [0,0,0]
+win_percent = []
+draw_percent = []
+lose_percent = []
 
 PLAYER_ID = 1
 for episode in range(EPISODES):
@@ -35,6 +43,25 @@ for episode in range(EPISODES):
         
         else:
             q_table[state + (action,)] = reward
+    draw_win_lose[reward] += 1
+    win_percent.append(draw_win_lose[1] / (episode+1))
+    lose_percent.append(draw_win_lose[-1] / (episode+1))
+    draw_percent.append(draw_win_lose[0] / (episode+1))
 
     env.display()
     print(f"Episode {episode}, win?: {env.is_winner(1)}")
+
+import matplotlib.pyplot as plt
+import os
+
+print(win_percent)
+
+x = range(EPISODES)
+plt.plot(x, win_percent, c='g')
+plt.plot(x, lose_percent, c='r')
+plt.plot(x, draw_percent, c='y')
+
+graph_dir_name = "Graphs"
+save_path = Path.cwd() / 'Exercise3' / graph_dir_name
+Path.mkdir(save_path, exist_ok=True)
+plt.savefig(f"{save_path}/q_learning_e={EPISODES}.png")
