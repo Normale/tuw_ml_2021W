7f8b63690fc8eeedcfcc2ecb77795ea69ff649b9
Normal <bartek.dybcio1@gmail.com>
2022-01-06 14:42:53 +0100

    Add q-tables save

--- Exercise3/RL/q_learning.py
+++ Exercise3/RL/q_learning.py
@@ -1,31 +1,31 @@
 import numpy as np
-from .environment import Environment
+from .environment import Environment, RandomPlayer
 from time import time, sleep
 from pathlib import Path
+import matplotlib.pyplot as plt
 
-EPISODES = 50000
+PLAYER_ID = 1
+EPISODES = 100000
 DISCOUNT = 0.9
 LEARNING_RATE = 0.4
 EPSILON = 0.5
-
+DISPLAY = False
 q_table = np.zeros(shape = (3,3,3,3,3,3,3,3,3,9))
-
-
 
 draw_win_lose = [0,0,0]
 win_percent = []
 draw_percent = []
 lose_percent = []
 
-PLAYER_ID = 1
 for episode in range(EPISODES):
-    env = Environment(1, -1, 0)
+    env = Environment(1, -1, 0, RandomPlayer(player_id=2))
     done = False
     while not done:
         state = env.get_state()
         while True:
             possible_actions = env.get_possible_moves()
-            if np.random.random() > EPSILON:
+            rand = np.random.random()
+            if rand > EPSILON:
                 action = np.argmax(q_table[state])
             else:
                 action = np.random.randint(0,9)
@@ -40,21 +40,19 @@ for episode in range(EPISODES):
             current_q = q_table[state + (action,)]
             new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
             q_table[state + (action,)] = new_q
-        
         else:
             q_table[state + (action,)] = reward
+
     draw_win_lose[reward] += 1
     win_percent.append(draw_win_lose[1] / (episode+1))
     lose_percent.append(draw_win_lose[-1] / (episode+1))
     draw_percent.append(draw_win_lose[0] / (episode+1))
+    if DISPLAY:
+        env.display()
+    print(f"Episode {episode}, rand: {rand <= EPSILON} win?: {env.is_winner(1)}")
 
-    env.display()
-    print(f"Episode {episode}, win?: {env.is_winner(1)}")
 
-import matplotlib.pyplot as plt
-import os
 
-print(win_percent)
 
 x = range(EPISODES)
 plt.plot(x, win_percent, c='g')
@@ -65,3 +63,8 @@ graph_dir_name = "Graphs"
 save_path = Path.cwd() / 'Exercise3' / graph_dir_name
 Path.mkdir(save_path, exist_ok=True)
 plt.savefig(f"{save_path}/q_learning_e={EPISODES}.png")
+
+qtables = "qtables"
+qtables_path = Path.cwd() / 'Exercise3' / qtables
+Path.mkdir(qtables_path, exist_ok=True)
+np.save(f"{qtables_path}/qtable-e={EPISODES}.npy", q_table)
