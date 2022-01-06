51540e57007566167bce9b8779b498b38daa0e62
Normal <bartek.dybcio1@gmail.com>
2022-01-06 17:18:53 +0100

    Add learning vs itself

--- Exercise3/RL/q_learning.py
+++ Exercise3/RL/q_learning.py
@@ -6,16 +6,13 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 PLAYER_ID = 1
-EPISODES = 100000
+EPISODES = 1000
 DISCOUNT = 0.9
-LEARNING_RATE = 0.4
-EPSILON = 0.5
+LEARNING_RATE = 0.7
+EPSILON = 0.2
 DISPLAY = True
 
-draw_win_lose = [0,0,0]
-win_percent = []
-draw_percent = []
-lose_percent = []
+
 
 graph_dir_name = "Graphs"
 save_path = Path.cwd() / 'Exercise3' / graph_dir_name
@@ -25,11 +22,20 @@ qtables = "qtables"
 qtables_path = Path.cwd() / 'Exercise3' / qtables
 Path.mkdir(qtables_path, exist_ok=True)
 
-def teach_qlearning(go_first: bool, enemy: Player, qtable_path: str = None):
-    if qtable_path is not None:
+def teach_qlearning(go_first: bool, enemy: Player, iteration: int = 0, q_table = None, qtable_path: str = None):
+    if qtable_path is None:
+        raise LookupError("Q-Table can not be found")
+
+    try:
         q_table = np.load(qtable_path)
-    else:
-        q_table = np.zeros(shape = (3,3,3,3,3,3,3,3,3,9))
+    except FileNotFoundError:
+        if not q_table:
+            q_table = np.zeros(shape = (3,3,3,3,3,3,3,3,3,9))
+
+    draw_win_lose = [0,0,0]
+    win_percent = []
+    draw_percent = []
+    lose_percent = []
 
     for episode in range(EPISODES):
         env = Environment(1, -1, 0, enemy, not go_first)
@@ -43,9 +49,9 @@ def teach_qlearning(go_first: bool, enemy: Player, qtable_path: str = None):
                     action = np.argmax(q_table[state])
                 else:
                     action = np.random.randint(0,9)
-                if action not in possible_actions:
-                    q_table[state + (action,)] = -10
-                else:
+                if action in possible_actions: #not in
+                    # q_table[state + (action,)] = -10
+                # else:
                     break
             new_state, reward, done = env.step(PLAYER_ID, action)
             current_q = q_table[state + (action,)]
@@ -63,16 +69,27 @@ def teach_qlearning(go_first: bool, enemy: Player, qtable_path: str = None):
         draw_percent.append(draw_win_lose[0] / (episode+1))
         if DISPLAY:
             env.display()
-        print(f"Episode {episode}, rand: {rand <= EPSILON} win?: {env.is_winner(1)}")
+        print(f"Episode {episode}, i {iteration} rand: {rand <= EPSILON} win?: {env.is_winner(1)}")
 
     x = range(EPISODES)
+    plt.clf()
     plt.plot(x, win_percent, c='g')
     plt.plot(x, lose_percent, c='r')
     plt.plot(x, draw_percent, c='y')
 
-    plt.savefig(f"{save_path}/q_learning_e={EPISODES}.png")
-    np.save(f"{qtables_path}/qtable-e={EPISODES}.npy", q_table)
+    plt.savefig(f"{save_path}/q_learning_e={EPISODES},i={iteration} en {enemy.get_player_name()}.png")
+    if qtable_path:
+        np.save(qtable_path, q_table)
+
+    # np.save(f"{qtables_path}/qtable-e={EPISODES}.npy", q_table)
 
 
 if __name__ == '__main__':
-    teach_qlearning(False, RandomPlayer(player_id=2))
+    filepath = r"Exercise3\qtables\qtable.npy"
+    teach_qlearning(True, RandomPlayer(player_id=2), qtable_path=filepath)
+    q_table = np.load(filepath)
+    ITERATIONS = 10
+    for i in range(ITERATIONS):
+        first = i % 2 == 0
+        teach_qlearning( first, QLPlayer(player_id=2, q_table=q_table), iteration=i, q_table=q_table, qtable_path = filepath)
+        teach_qlearning( first, QLPlayer(player_id=2, q_table=q_table), iteration=i, q_table=q_table, qtable_path = filepath)
